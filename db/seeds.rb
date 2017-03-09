eec9b23337e4dadcb1a36a6ddba302c8356cb177
Rakesh <rajwar.rakesh@gmail.com>
2017-03-09 09:10:04 +0530

    disease table and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Diseases_1.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|	
+  t = Disease.new
+  t.name = row['name']
+  t.save
+end
+
+puts "There are now #{Disease.count} rows in the transactions table"
