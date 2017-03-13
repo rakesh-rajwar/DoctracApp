f6b49720371b48bdbcfd3ee0c4a5a962ffb8e502
Rakesh <rajwar.rakesh@gmail.com>
2017-03-13 22:38:54 +0530

    apis for medical history search and add

--- app/controllers/api/v1/diseases_controller.rb
+++ app/controllers/api/v1/diseases_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::DiseasesController < ApiController
   end
 
   def add_to_patient
-  	diseases = Disease.where(id: params[:id])
-  	current_resource_owner << diseases
-  	render json: responseMessage: "Diseases added to your profile successfully.", status: :ok 
+  	diseases = Disease.where(id: params[:ids])
+  	current_resource_owner.diseases << diseases
+  	render json: {responseMessage: "Diseases added to your profile successfully."}
   end
 end
--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,5 +1,9 @@
 class Disease < ApplicationRecord
   has_and_belongs_to_many :users
   include PgSearch
-  pg_search_scope :search_by_name, :against => :name
+  pg_search_scope :search_by_name, 
+  				  :against => :name,
+                  :using => {
+                    :tsearch => {:prefix => true}
+                  }
 end
